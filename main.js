daee1466643cf9c9bf72cf8fe8cbf96438d8e168
ushica <85010308+ushica@users.noreply.github.com>
2022-02-15 20:54:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var Textbox = document.getElementById("textbox")
+
+function start() {
+Textbox.innerHTML = "";
+recognition.start();    
+}
+
+recognition.onresult = function(event) {
+console.log(event);
+var Content = event.results[0][0].transcript;
+Textbox.innerHTML = Content;
+console.log(Content);
+if(Content =="take my selfie")
+{
+console.log("taking selfie --- ");
+speak();
+}
+    
+function speak(){
+var synth = window.speechSynthesis;
+speak_data = "Taking you Selfie in 5 seconds";
+var utterThis = new SpeechSynthesisUtterance(speak_data)
+synth.speak(utterThis);
+Webcam.attach(camera);
+    
+    setTimeout(function()
+    {
+    take_snapshot();
+    save();
+    }, 5000);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+Webcam.snap(function(data_url) {
+document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_url+'"/>';
+});
+}
+
+function save()
+{
+link = document.getElementById("link");
+image = document.getElementById("selfie_image").src;
+link.href = image;
+link.click();
+}
